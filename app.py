74d6bd6439caa3b897b0306c02e7ee9efa3bf49d
Yatharth-byte <78674002+Yatharth-byte@users.noreply.github.com>
2021-06-22 15:59:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,22 @@ st.header("Please use proper spelling!")
 
 iput = st.text_input("Enter Text:")
 oput_dict =  analyzer.polarity_scores(iput)
+col1, col2, col3 = st.beta_columns(3)
 
 if st.button('Analyze'):
+  with col1:
+    st.header('**Negative** :angry:')
+    st.header(int(oput_dict['neg']*100))
+  with col2:
+    st.header('**Neutral** :unamused:')
+    st.header(int(oput_dict['neu']*100))
+  with col3:
+    st.header('**Positive** :smile:')
+    st.header(int(oput_dict['pos']*100))
   if oput_dict['compound'] >= 0.05:
-    st.write(' *Positive* :smile:')
+    st.write("Its ",int(oput_dict['pos']*100),"% Positive.")
   elif oput_dict['compound'] <= -0.05:
-    st.write('*Negative* :angry:')
+    st.write("Its ",int(oput_dict['neg']*100),"% Negative.")
   else:
-    st.write('*Neutral* :unamused:') 
+    st.write("Its ",int(oput_dict['neu']*100),"% Neutral.") 
  
